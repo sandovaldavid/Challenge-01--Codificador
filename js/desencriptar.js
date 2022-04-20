0e8b3cc9a91f56f78a41f2aa5bd3561b48c15647
David <sandovaldavid2201@gmail.com>
2022-04-20 11:44:28 -0500

    Optimize el codigo e implemente nuevas funciones

--- js/desencriptar.js
+++ js/desencriptar.js
@@ -1,7 +1,5 @@
 var b_desencriptar  = document.querySelector("#desencriptar");
 var b_encriptar = document.querySelector("#encriptar");
-var ta_texto = document.querySelector("#texto");
-var panel_img_mensaje = document.querySelector(".panel-img-mensaje");
 var panel_2 = document.querySelector(".panel-2");
 var valor_b_desencriptar = false;
 
@@ -23,16 +21,9 @@ b_desencriptar.addEventListener("click", function(){
         construirBotonCopiar();
         valor_b_encriptar = true;
     }
-    panel_img_mensaje.classList.add("fadeOut");
-    setTimeout(function(){
-        panel_img_mensaje.classList.add("invisible");
-    },200);
-
+    invisible_panel_img_mensaje();
     botonCopiar();
-    var b_copiar = document.querySelector("#copiar");
-    var texto_panel_2 = document.querySelector(".texto-panel-2");
-    texto_panel_2.classList.remove("fadeOut");
-    b_copiar.classList.remove("fadeOut");
+    Aparicion_Botones_texto_panel_2();
 });
 
 function desencriptar_texto(texto_encriptado){  
