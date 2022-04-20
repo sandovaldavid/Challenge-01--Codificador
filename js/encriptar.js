050dbd65e1174bf259b733667dfdc1b924101cad
David <sandovaldavid2201@gmail.com>
2022-04-20 18:35:48 -0500

    Implemeente la nueva funcion para validar texto vacio

--- js/encriptar.js
+++ js/encriptar.js
@@ -5,20 +5,22 @@ var valor_b_encriptar = false;
 b_encriptar.addEventListener("click", function(){
     var texto = document.getElementById("texto").value;
     document.getElementById("texto").value = "";
-    if(valor_b_encriptar){
-        var p = document.querySelector(".texto-panel-2");
-        var b_copiar = document.querySelector("#copiar");
-        p.innerHTML = encriptar_texto(texto);
-        p.classList.remove("invisible");
-        b_copiar.classList.remove("invisible");
-    }else{
-        panel_2.appendChild(construir_P(encriptar_texto(texto),"texto-panel-2"));
-        construirBotonCopiar();
-        valor_b_encriptar = true;
+    if(texto_vacio(texto)){
+        if(valor_b_encriptar){
+            var p = document.querySelector(".texto-panel-2");
+            var b_copiar = document.querySelector("#copiar");
+            p.innerHTML = encriptar_texto(texto);
+            p.classList.remove("invisible");
+            b_copiar.classList.remove("invisible");
+        }else{
+            panel_2.appendChild(construir_P(encriptar_texto(texto),"texto-panel-2"));
+            construirBotonCopiar();
+            valor_b_encriptar = true;
+        }
+        invisible_panel_img_mensaje();
+        botonCopiar();
+        Aparicion_Botones_texto_panel_2();
     }
-    invisible_panel_img_mensaje();
-    botonCopiar();
-    Aparicion_Botones_texto_panel_2();
 });
 
 function encriptar_texto(texto){    
