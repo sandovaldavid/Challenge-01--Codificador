9fcc6292692373ecc1b83bcbb88e8caca2a7278b
David <sandovaldavid2201@gmail.com>
2022-04-19 19:14:10 -0500

    Implemente las funciones para mi boton encriptar

---
+++ js/encriptar.js
@@ -0,0 +1,52 @@
+var b_encriptar  = document.querySelector("#encriptar");
+var ta_texto = document.querySelector("#texto");
+var panel_img_mensaje = document.querySelector(".panel-img-mensaje");
+var panel_2 = document.querySelector(".panel-2");
+
+b_encriptar.addEventListener("click", function(){
+
+    var texto = document.getElementById("texto").value;
+    document.getElementById("texto").value = "";
+    panel_img_mensaje.classList.add("invisible");
+    panel_2.appendChild(construir_P(encriptar_texto(texto),"texto-panel-2"))
+});
+
+function encriptar_texto(texto){    
+    var i = 0;
+    var mensaje_encriptado = "";
+    var tm_texto = texto.length;
+
+    while(i < tm_texto){
+        var letra = texto.charAt(i);
+
+        if(letra == "a"){
+            mensaje_encriptado = mensaje_encriptado+"ai";
+        }
+        else if(letra == "e"){
+            mensaje_encriptado = mensaje_encriptado+"enter";
+        }
+        else if(letra == "i"){
+            mensaje_encriptado = mensaje_encriptado+"imes";
+        }
+        else if(letra == "o"){
+            mensaje_encriptado = mensaje_encriptado+"ober";
+        }
+        else if(letra == "u"){
+            mensaje_encriptado = mensaje_encriptado+"ufat";
+        }
+        else{
+            mensaje_encriptado = mensaje_encriptado+letra;
+        }
+
+        i++;
+    }
+
+    return mensaje_encriptado;
+}
+
+function construir_P(texto,clase){
+    var p = document.createElement("p");
+    p.classList.add(clase);
+    p.textContent = texto;
+    return p;
+}
