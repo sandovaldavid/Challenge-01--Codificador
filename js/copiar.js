9da0eba5eb4374b535e5fd8504994fef2505b92f
David <sandovaldavid2201@gmail.com>
2022-04-20 00:41:56 -0500

    Agregue esta funcion copy para copiar el contenido de la etiqueta <p>

--- js/copiar.js
+++ js/copiar.js
@@ -6,3 +6,13 @@ function construirBotonCopiar(){
     b_copiar.classList.add("boton");
     panel_2.appendChild(b_copiar);
 }
+
+function copy(texto) {
+    var input =document.createElement("input");
+    input.setAttribute("value",texto.innerText);
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand("copy");
+    input.remove();
+    panel_boton_img.appendChild(panel_img_texpanel_img_tex);
+}
