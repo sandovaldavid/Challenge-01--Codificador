8890a59d39ec3b65ce27905ed116548ed36176e4
Juan David Sandoval Salvador <sandovaldavid2201@gmail.com>
2022-04-21 22:39:54 -0500

    Impelemnte mi funcion para validar texto

--- js/encriptar.js
+++ js/encriptar.js
@@ -4,52 +4,47 @@ var valor_b_encriptar = false;
 
 b_encriptar.addEventListener("click", function(){
     var texto = document.getElementById("texto").value;
+    var T_encriptado = Validar_Texto(texto);
     document.getElementById("texto").value = "";
+
     if(texto_vacio(texto)){
         if(valor_b_encriptar){
             var p = document.querySelector(".texto-panel-2");
             var b_copiar = document.querySelector("#copiar");
-            p.innerHTML = encriptar_texto(texto);
+            p.innerHTML = T_encriptado;
             p.classList.remove("invisible");
             b_copiar.classList.remove("invisible");
         }else{
-            panel_2.appendChild(construir_P(encriptar_texto(texto),"texto-panel-2"));
+            panel_2.appendChild(construir_P(T_encriptado,"texto-panel-2"));
             construirBotonCopiar();
             valor_b_encriptar = true;
         }
         Aparicion_Botones_texto_panel_2();
         botonCopiar();
+        Texto_invalido()
     }
 });
 
-function encriptar_texto(texto){    
-    var i = 0;
+function encriptar_texto(letra){    
     var mensaje_encriptado = "";
-    var tm_texto = texto.length;
 
-    while(i < tm_texto){
-        var letra = texto.charAt(i);
-
-        if(letra == "a"){
-            mensaje_encriptado = mensaje_encriptado+"ai";
-        }
-        else if(letra == "e"){
-            mensaje_encriptado = mensaje_encriptado+"enter";
-        }
-        else if(letra == "i"){
-            mensaje_encriptado = mensaje_encriptado+"imes";
-        }
-        else if(letra == "o"){
-            mensaje_encriptado = mensaje_encriptado+"ober";
-        }
-        else if(letra == "u"){
-            mensaje_encriptado = mensaje_encriptado+"ufat";
-        }
-        else{
-            mensaje_encriptado = mensaje_encriptado+letra;
-        }
-
-        i++;
+    if(letra == "a"){
+        mensaje_encriptado = "ai";
+    }
+    else if(letra == "e"){
+        mensaje_encriptado = "enter";
+    }
+    else if(letra == "i"){
+        mensaje_encriptado = "imes";
+    }
+    else if(letra == "o"){
+        mensaje_encriptado = "ober";
+    }
+    else if(letra == "u"){
+        mensaje_encriptado = "ufat";
+    }
+    else{
+        mensaje_encriptado = letra;
     }
 
     return mensaje_encriptado;
